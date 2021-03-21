fc9700cbdd3a22f0613e296e2db9fef975fe72d7
El Beni <benrivarr@outlook.com>
2021-03-21 15:08:40 -0600

    avance

--- Quizz App (2)v1/src/public/js/creadorQuizzes.js
+++ Quizz App (2)v1/src/public/js/creadorQuizzes.js
@@ -480,38 +480,7 @@ function construirObjLienzo(espacioObj,datos){
 }
 
 
-function previsualizarIT(){
-  var idFile="imgIT"+contadorPreV;
-  var idpreView="prevIT"+contadorPreV;
-  //Función para crear las funciones preview
-document.getElementById(idFile).onchange = function (e) {
-  // Creamos el objeto de la clase FileReader
-  let reader = new FileReader();
 
-  // Leemos el archivo subido y se lo pasamos a nuestro fileReader
-  reader.readAsDataURL(e.target.files[0]);
-
-  // Le decimos que cuando este listo ejecute el código interno
-  reader.onload = function () {
-    let preview = document.getElementById(idpreView),
-      image = document.createElement("img");
-    image.src = reader.result;
-
-        
-     //Si ya hay una imagen
-     if(preview.children[0]){
-      preview.children[0].remove()
-      preview.appendChild(image)
-    }
-    else{
-      preview.appendChild(image);
-    }
-   
-  }
-    
-   
-  };
-}
 function previsualizarDraw(){
 //Función para crear las funciones preview
 document.getElementById("file").onchange = function (e) {
@@ -651,6 +620,8 @@ document.getElementById("file").onchange = function (e) {
             strVar += "    <\/div>";
             strVar += "  <\/div>";
       insertor.insertAdjacentHTML("beforeend",strVar);   
+
+      previsualizarDraw
       }
 
 
