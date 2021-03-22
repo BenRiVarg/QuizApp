8a4aa23259b7d4679b1f83926cc28b6971b00353
Alex G <glopezalex631@gmail.com>
2021-03-22 01:22:04 -0600

    gitflow-feature-stash: califglobal

--- Quizz App (2)v1/src/modelos/registros.js
+++ Quizz App (2)v1/src/modelos/registros.js
@@ -1,23 +1,24 @@
 'use strict'
 
-const mongoose=require('mongoose');
-const Schema= mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const respuestas=Schema({
-    cuestionarioID:{type: 'String'},
+const respuestas = Schema({
+    cuestionarioID: { type: 'String' },
     respuestaA: Array,
     revision: Array
 });
 
 
-var modeloRegistro = Schema ({
-    alumno:{type:String},
-    revisado:{type: Boolean, default: false },
-    quizz: {type:String},
-    respuestas:[respuestas]
+var modeloRegistro = Schema({
+    alumno: { type: String },
+    revisado: { type: Boolean, default: false },
+    quizz: { type: String },
+    calificacion: { type: Number },
+    respuestas: [respuestas]
 });
 
 
 
 module.exports =
- mongoose.model('Registros',modeloRegistro);
+    mongoose.model('Registros', modeloRegistro);
